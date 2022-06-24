9acc6fe55c53d766900ed525bc4b6425d3e33290
Joao Medeiros <jmedeiro@redhat.com>
2022-06-24 11:01:44 -0300

    Configuring redhat dependency management

---
+++ src/main/java/com/redhat/springtools/RedhatApplication.java
@@ -0,0 +1,13 @@
+package com.redhat.springtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedhatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedhatApplication.class, args);
+	}
+
+}
